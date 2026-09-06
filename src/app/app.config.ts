64d6319f30255919089d1947eabbce13b4b8e392
hexed-AAL1X <205622515+hexed-AAL1X@users.noreply.github.com.>
2026-09-06 09:27:25 -0500

    ya esta eso creo, muchos errores tenia esta mrd

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,18 +4,19 @@ import {
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
 } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { icons, LucideAngularModule } from 'lucide-angular';
 
 import { routes } from './app.routes';
+import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authInterceptor])),
     provideAnimations(),
     importProvidersFrom(LucideAngularModule.pick(icons)),
     provideRouter(routes),
