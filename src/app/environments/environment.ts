b1e88331ea6c9477905694aa5d26df6766c23318
hexed-AAL1X <205622515+hexed-AAL1X@users.noreply.github.com.>
2026-09-06 09:08:52 -0500

    Pas@r de Pr0duct aaa Dev, URL CAMBIADA

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  //apiUrl: 'https://financello-api.onrender.com/api/v1',
+  //apiUrl: 'https://financello-api-vfq9.onrender.com/api/v1',
   apiUrl: 'http://localhost:8080/api/v1',
   version: '1.0.0',
 };
